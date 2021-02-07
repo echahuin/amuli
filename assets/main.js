cbf33eb37f2a43ff64a46624e1bed56dd0b4f35b
Edgarr <edgachahuinauccafuro@utn.frba.utn.edu.ar>
2021-02-06 22:04:55 -0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,40 @@
+//------------------- aqui tenemos todo el codigo para hacer un slider automatico
+
+let slider = document.querySelector(".containerSlider");
+let sliderIndividual = document.querySelectorAll('.slider');
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+let widthPC = 416;
+
+console.log("slider", slider, "sliderIndividual", sliderIndividual, "contador", contador, "width", width, "intervalo", intervalo)
+
+// esete metodo ara que se haga un calculo del ancho de la pantalla
+window.addEventListener("resize", function () {
+    width = sliderIndividual[0].clientWidth;
+})
+function slides() {
+
+    slider.style.transform = "translate(" + (-width * contador) + "px)";
+    console.log('this is width', width)
+    /*slider.style.transform = `translate(-${width}*${contador}px)`*/
+    slider.style.transition = "transform 1s"
+    contador++;
+    if (width === widthPC && contador == sliderIndividual.length - 3) {
+        contador = 0;
+    }
+
+    if (contador == sliderIndividual.length) {
+
+        setTimeout(function () {
+            slider.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador = 0;
+        })
+
+    }
+}
+
+setInterval(function () {
+    slides()
+}, intervalo);
